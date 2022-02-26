71988658e79e6631938b95353090aa3a4ad6f978
Ahlem <boucah@hotmail.com>
2022-02-26 19:55:15 +0100

    Finished challenge 3

--- Level 1/Challenge 3/validation.js
+++ Level 1/Challenge 3/validation.js
@@ -6,10 +6,13 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit',(e) => {
-  e.preventDefault();
-  checkInputs();
-});
+
+form.addEventListener('click',(e) => {
+    e.preventDefault();
+    checkInputs();
+  });
+
+
 
 function checkInputs(){
   //get values from the inputs
@@ -18,6 +21,7 @@ function checkInputs(){
   const passwordValue=password.value.trim()
   const password2Value=password2.value.trim()
 
+  // Handling username field
   if(usernameValue === ''){
     // show error
     // add error class
@@ -27,12 +31,55 @@ function checkInputs(){
     //add success class
     setSuccessFor(username);
   }
+
+  // Handling email field
+  if(emailValue === ''){
+    setErrorFor(email, 'Email cannot be blank');
+  } else if (!isValidEmail(emailValue)){
+    setErrorFor(email, 'Email is not valid')
+
+  }else{
+    setSuccessFor(email);
+  }
+
+  // Handling password field
+  if(passwordValue === ''){
+    setErrorFor(password, 'Password cannot be empty');
+  }else if(passwordValue.length <= 8){
+    setErrorFor(password,'Password too short')
+  }else{
+    setSuccessFor(password);
+  }
+
+  // Handling password check field
+  if(password2Value === ''){
+    setErrorFor(password2, 'Confirm your password!');
+  }else if(passwordValue !== password2Value){
+    setErrorFor(password2, 'Passwords do not match')
+  }else{
+    setSuccessFor(password);
+  }
 }
+
 
 function setErrorFor(input, message){
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector('small');
 
+  //add error message inside small
+  small.innerText = message;
+
+  // add error class
+  formControl.className = 'form-control error';
 }
 
-function setSuccessFor(input, message){
-  
+function setSuccessFor(input){
+  const formControl = input.parentElement; //.form-control
+
+  // add success class
+  formControl.className = 'form-control success';
+}
+
+function isValidEmail(email){
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 }
